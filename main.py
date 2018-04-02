b9fbad1dcaab68d9ccf69bac417d6487df31a4e2
Michal Michalski <buyuk.dev@gmail.com>
2018-04-02 12:24:54 +0200

    initialize project

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+
+import numpy
+import matplotlib
+import skimage
+
+
+
+DETECTORS = [
+
+]
+
+
+
+def cast_ray(angle, detector)
+
+
+
+
+if __name__ == '__main__':
+    print("Hello, world!")
