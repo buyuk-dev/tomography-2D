319553340267a0aac1f6a2b67e0a873e34e25b09
Michal Michalski <buyuk.dev@gmail.com>
2018-04-02 13:14:08 +0200

    loading greyscale image

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 #!/usr/local/bin/python3
 
+import math
 import numpy
 import matplotlib
-import skimage
+import skimage.io
 
 
+RESOLUTION_D = 100                     # number of detectors
+RESOLUTION_A = 360                     # number of angular samples
+
+INIT_ANGLE = math.pi / 2.0
 
 DETECTORS = [
-
+    position_of(i, INIT_ANGLE)
 ]
 
 
+def load_object(filename):
+    return skimage.io.imread(filename, as_grey=True)
 
-def cast_ray(angle, detector)
+
+def cast_ray(image, angle, detector):
+    return 0.5
+    
 
 
 
