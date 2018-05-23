40db1e51cda9b1fa5fed50b2f2ce1419992f80fe
Michal Michalski <buyuk.dev@gmail.com>
2018-05-23 23:02:20 +0200

    remove old, faulty implementation of segment rasterization

--- bresenham.py
+++ bresenham.py
@@ -55,27 +55,3 @@ def plotLine(x0,y0, x1,y1):
 def bresenham_segment(A, B):
     return plotLine(A.x, A.y, B.x, B.y)
 
-
-def bresenham_segment_old(A, B):
-    if A.x > B.x:
-        tmp = A
-        A = B
-        B = tmp
-
-    line = mathutils.get_line_params(A, B)
-
-    path = []
-    for x in mathutils.closed_range(A.x, B.x, 1):
-        if line is None:
-            direction = 1
-            if A.y > B.y:
-                direction = -1
-            for y in mathutils.closed_range(A.y, B.y, direction):
-                path.append((x, y))
-        else:
-            yr = line[0] * x + line[1]
-            y = round(yr)
-            path.append((x, y))
-
-    return path
-
